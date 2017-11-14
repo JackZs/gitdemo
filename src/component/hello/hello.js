99e005b56d52b36fcc3874097d868c3ece6ddbbc
Jack <270264089@qq.com>
2017-11-14 23:02:15 +0800

    react项目

---
+++ src/component/hello/hello.js
@@ -0,0 +1,6 @@
+import React,{Component} from 'react';
+export default class Hello extends Component{
+	render(){
+   return (<h3>hello react11111!</h3>)
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+import ReactDom from 'react-dom';
+import Hello from './component/hello/hello'
+ReactDom.render(<Hello/>,document.getElementById("app"))
+import Obj112221 from './component/a.js'
+// console.log(Component)
+// console.log(React)
+console.log(Obj112221 )
